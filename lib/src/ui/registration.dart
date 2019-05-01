aa5a199f5c09476cc9df857a0947a1999deb39b0
Stefan Eggenschwiler <stefan.eggenschwiler@hevs.ch>
2019-05-01 09:08:11 +0200

    Added stepper implementation

--- lib/src/ui/registration.dart
+++ lib/src/ui/registration.dart
@@ -3,16 +3,51 @@ import 'package:flutter/material.dart';
 
 import 'nav_drawer.dart';
 
-class Registration extends StatelessWidget {
+class Registration extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return RegistrationState();
+  }
+}
+
+class RegistrationState extends State<Registration> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavDrawer(AppTranslations.of(context).text('tab_registration')),
-      appBar: new AppBar(
-        title: new Text(AppTranslations.of(context).text('tab_registration')),
-      ),
-      body: Text(AppTranslations.of(context).text('txt_about')),
-    );
+        appBar: AppBar(
+          title: Text(AppTranslations.of(context).text('tab_registration')),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                child: Stepper(
+                  steps: [
+                    Step(
+                      title: Text("First"),
+                      subtitle: Text("This is our first article"),
+                      content: Text(
+                          "In this article, I will tell you how to create a page."),
+                    ),
+                    Step(
+                        title: Text("Second"),
+                        subtitle: Text("Constructor"),
+                        content: Text("Let's look at its construtor."),
+                        state: StepState.editing,
+                        isActive: true),
+                    Step(
+                        title: Text("Third"),
+                        subtitle: Text("Constructor"),
+                        content: Text("Let's look at its construtor."),
+                        state: StepState.error),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
